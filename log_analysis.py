bf96c8558e2213a8e064f0824cca77d3f68a47d6
Sofiane DERRAZ <40466424+DerrazSofiane@users.noreply.github.com>
2021-11-16 14:04:04 +0100

    removed beta_

--- log_analysis.py
+++ log_analysis.py
@@ -11,12 +11,12 @@ def app():
 
     st.subheader('Raw Data')
     log_data = pd.read_csv("log.csv")
-    col1, col2 = st.beta_columns(2)
+    col1, col2 = st.columns(2)
     with col1:
-        with st.beta_expander("log.csv"):
+        with st.expander("log.csv"):
             st.dataframe(log_data)
     with col2:
-        with st.beta_expander("columns"):
+        with st.expander("columns"):
             st.text("Columns explanation:")
             body = """
             * **url**: url of the product page.
@@ -50,13 +50,13 @@ def app():
     error_count_by_function = error_count_by_function.sort_values(
         by='error_message', ascending=False)
 
-    col1_, col2_ = st.beta_columns(2)
+    col1_, col2_ = st.columns(2)
     with col1_:
-        with st.beta_expander("Error count by function"):
+        with st.expander("Error count by function"):
             st.dataframe(error_count_by_function)
 
     with col2_:
-        with st.beta_expander("Bar chart comparison"):
+        with st.expander("Bar chart comparison"):
             error_count_by_function = error_count_by_function[
                 error_count_by_function["error_message"]>0]
             bar_fig = px.bar(
@@ -65,7 +65,7 @@ def app():
                 y=["error_message"])
             st.plotly_chart(bar_fig, use_container_width=True)
 
-    with st.beta_expander("Pie chart comparison"):
+    with st.expander("Pie chart comparison"):
         error_count = log_data.groupby(
             ['function_name']).count()[["error_message"]].reset_index()
         error_count = error_count[error_count["error_message"]>0]
@@ -83,7 +83,7 @@ def app():
     log_data["error_message"] = log_data["error_message"].str.split("byte").str[0]
 
     st.subheader('What are the error messages?')
-    with st.beta_expander("Errors detail"):
+    with st.expander("Errors detail"):
         error_count_by_message = log_data['error_message'].value_counts()
         error_count_by_message = error_count_by_message.rename_axis('error_message')
         error_count_by_message = error_count_by_message.reset_index(name='counts')
@@ -91,7 +91,7 @@ def app():
             error_count_by_message["counts"]>1]
         st.dataframe(error_count_by_message)
 
-    with st.beta_expander("Pie chart comparison"):
+    with st.expander("Pie chart comparison"):
         pie_fig2 = px.pie(
             error_count_by_message,
             values='counts',
@@ -103,7 +103,7 @@ def app():
     error_count_by_message["percentage_over_all_urls"] = error_count_by_message["counts"] \
         *100/len(log_data["url"].unique())
 
-    with st.beta_expander("Percentage of occurence"):
+    with st.expander("Percentage of occurence"):
         st.dataframe(error_count_by_message[[
         "error_message", "percentage_over_all_urls"]])
 
@@ -112,20 +112,20 @@ def app():
         "/shopping/product/", na=False)]
     log_data_grouped = log_data_grouped.groupby([
         'function_name', 'error_message', 'error_level']).count()[["url"]]
-    with st.beta_expander("Function with its error message"):
+    with st.expander("Function with its error message"):
         st.dataframe(log_data_grouped)
 
     st.subheader('To which urls are the diffbot error messages linked?')
     problematic_urls = log_data["url"][log_data["error_message"].str.contains(
         "objects", na=False)]
-    with st.beta_expander("Problematic urls"):
+    with st.expander("Problematic urls"):
         st.write("len:", len(problematic_urls))
         for url in problematic_urls:
             st.write(url)
 
     st.header('Study of processing time of functions')
     st.subheader('Global time taken')
-    with st.beta_expander("Time (in second) distribution of the main function"):
+    with st.expander("Time (in second) distribution of the main function"):
         main_function_data = log_data[log_data["function_name"].isin(
             ["get_product_vs_replicas"])]
 
@@ -135,7 +135,7 @@ def app():
         st.plotly_chart(fig1, use_container_width=True)
 
     st.subheader('Diffbot vs Old scraper')
-    with st.beta_expander("Time (in second) distribution of the diffbot function and old scraper function"):
+    with st.expander("Time (in second) distribution of the diffbot function and old scraper function"):
 
         diffbot_data = log_data["process_time"][log_data["function_name"].isin(["data_Diffbot"])].to_list()
         old_scraper_data = log_data["process_time"][log_data["function_name"].isin(["get_data"])].to_list()
@@ -146,7 +146,7 @@ def app():
         st.plotly_chart(fig2, use_container_width=True)
 
     st.subheader('Old scraper detail processing time')
-    with st.beta_expander("Time (in second) distribution of each function that compose old scraper function"):
+    with st.expander("Time (in second) distribution of each function that compose old scraper function"):
         
         old_sc_func_1 = log_data["process_time"][log_data["function_name"].isin(["get_name"])].to_list()
         old_sc_func_2 = log_data["process_time"][log_data["function_name"].isin(["get_description"])].to_list()
@@ -160,7 +160,7 @@ def app():
         st.plotly_chart(fig3, use_container_width=True)
 
     st.subheader('Aliexpress vs Google')
-    with st.beta_expander("Time (in second) distribution of the replicas collector functions"):
+    with st.expander("Time (in second) distribution of the replicas collector functions"):
         
         func_1 = log_data["process_time"][log_data["function_name"].isin(["data_Aliexpress"])].to_list()
         func_2 = log_data["process_time"][log_data["function_name"].isin(["data_Google_combined"])].to_list()
@@ -171,7 +171,7 @@ def app():
         st.plotly_chart(fig4, use_container_width=True)
 
     st.subheader('Google Vision')
-    with st.beta_expander("Time (in second) distribution of the google image classification function"):
+    with st.expander("Time (in second) distribution of the google image classification function"):
         
         goo_func_1 = log_data["process_time"][log_data["function_name"].isin(["dataset_Google_Vision"])].to_list()
         goo_func_2 = log_data["process_time"][log_data["function_name"].isin(["data_Google_Vision"])].to_list()
