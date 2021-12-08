52a0734aba109b12b5495fa504b763492ad9c4d8
Sofiane DERRAZ <40466424+DerrazSofiane@users.noreply.github.com>
2021-12-08 13:37:59 +0100

    Add files via upload

--- debugger.py
+++ debugger.py
@@ -1,5 +1,10 @@
+import streamlit as st
 import streamlit.components.v1 as components
+import webbrowser
+
 
 
 def app():
-    components.iframe("http://161.35.202.8:8050/")
+    url = "http://161.35.202.8:8050/"
+    if st.button('Open debugger'):
+        webbrowser.open_new_tab(url)
--- labelstudio.py
+++ labelstudio.py
@@ -1,5 +1,9 @@
+import streamlit as st
 import streamlit.components.v1 as components
+import webbrowser
 
 
 def app():
-    components.iframe("http://161.35.202.8:8080")
+    url = "http://161.35.202.8:8080"
+    if st.button('Open label studio'):
+        webbrowser.open_new_tab(url)
