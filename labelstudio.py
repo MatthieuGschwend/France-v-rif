b4c9f4f3c343fd00209790fe498979f0921fc711
Sofiane DERRAZ <40466424+DerrazSofiane@users.noreply.github.com>
2021-12-09 09:59:41 +0100

    Add files via upload

--- labelstudio.py
+++ labelstudio.py
@@ -1,8 +1,26 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import webbrowser
+import requests
+
+
+def post_to_label_studio_app(text):
+    requests.post(
+        "http://161.35.202.8:8080/api/projects/1/import",
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Token 2860c48100a92de0c80ccca21751a17e1a79ec08"
+        },
+        json=[{"text": text}]
+    )
 
 
 def app():
-    link = '[label studio](http://161.35.202.8:8080/)'
-    st.markdown(link, unsafe_allow_html=True)
+    form = st.form(key='label form')
+    text_input = form.text_input(label='Enter some text')
+    submit_button = form.form_submit_button(label='Submit')
+    if submit_button:
+        try:
+            post_to_label_studio_app(text_input)
+        except Exception as e:
+            st.error(e)
+        st.success('Successfully sent to the label sutdio app!')
